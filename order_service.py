47e1f0fc64787c0d68efa4927a4e880bdf982518
Hever Alfonso <haalfonsoj@eafit.edu.co>
2026-09-23 11:30:42 -0500

    Agregar .gitignore y completar create_order

--- order_service.py
+++ order_service.py
@@ -1,19 +1,19 @@
 from models import Order
 
 def create_order(user_id, amount, notifier, logger, db, user_repository):
-    # TODO: obtener el email desde el repositorio
-    email = ________________________________
+    # obtiene el email desde el repositorio recibido (puede ser real, fake o stub)
+    email = user_repository.get_user_email(user_id)
     logger.log(f'Creating order for {email}')
-   
-   # TODO: validar que amount sea positivo, de lo contrario lanzar el error "Invalid amount"
-    ______________________
-    ______________________
-    
+
+    # valida que el monto sea positivo antes de crear la orden
+    if amount <= 0:
+        raise ValueError("Invalid amount")
+
     order = Order(user_email=email, amount=amount, status='CREATED')
-    
-    # TODO: persistir la orden
-    ________________________________
+
+    # registra la orden en la sesion y la persiste en la base de datos
+    db.add(order)
     db.commit()
-    
+
     notifier.send(email, 'Order created')
     return order
