f81d2ff811ff66b867855b83560b284ca26203ab
Hever Alfonso <haalfonsoj@eafit.edu.co>
2026-09-24 07:23:01 -0500

    Completar stub y asserts en test unitario

--- test_unit_with_stub.py
+++ test_unit_with_stub.py
@@ -4,8 +4,8 @@ from models import Order
 
 class StubUserRepository:
     def get_user_email(self, user_id):
-        # TODO: retornar siempre el mismo email ficticio
-        return "_____________________"
+        # siempre devuelve el mismo email fijo, sin importar el user_id
+        return "stub@test.com"
 
 class DummyLogger:
     def log(self, msg):
@@ -18,9 +18,9 @@ class NullNotifier:
 def test_create_order_with_stub():
     db = SessionLocal()
     order = create_order(10, 200, NullNotifier(), DummyLogger(), db, StubUserRepository())
-    # TODO: Comparar con las respuestas esperadas
-    assert order.status == '______'
-    assert order.user_email == "_____________________"
+    # el estado y el email deben coincidir con lo que devuelve el stub
+    assert order.status == 'CREATED'
+    assert order.user_email == "stub@test.com"
     
     db.query(Order).delete()
     db.commit()
